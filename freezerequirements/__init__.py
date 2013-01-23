ae9b2e1d0f0c9524921a38dbfaf8df7cc2aa4ace
Luper Rouch <luper.rouch@gmail.com>
2013-01-23 23:48:35 +0100

    printing all except final frozen contents to stderr (not quite usefull yet as fabric prints its logs to stdout currently)

--- freezerequirements/__init__.py
+++ freezerequirements/__init__.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import os
 import sys
 import atexit
@@ -19,6 +21,7 @@ except ImportError:
 
 
 TEMPFILES_PREFIX = 'freeze-requirements-'
+SEPARATOR = '-' * 78
 
 
 def main():
@@ -38,14 +41,15 @@ def main():
 
     # Verify options
     if not options.output and not options.upload:
-        print 'You must specify either --upload or --output'
+        print('You must specify either --upload or --output', file=sys.stderr)
         sys.exit(1)
     if options.output and options.remote_pip:
-        print "You can't use --output with --remote-pip"
+        print("You can't use --output with --remote-pip", file=sys.stderr)
         sys.exit(1)
     if options.output:
         if not op.isdir(options.output):
-            print 'Output directory does not exist: %s' % options.output
+            print('Output directory does not exist: %s' % options.output, 
+                    file=sys.stderr)
             sys.exit(1)
         output_dir = options.output
     else:
@@ -54,40 +58,43 @@ def main():
 
     if options.upload:
         if not fabric_present:
-            print 'You need to install fabric to use --upload'
+            print('You need to install fabric to use --upload', 
+                    file=sys.stderr)
             sys.exit(1)
         try:
             env.host_string, remote_dir = options.upload.split(':', 1)
         except ValueError:
-            print 'Invalid upload destination: %s' % options.upload
+            print('Invalid upload destination: %s' % options.upload, 
+                    file=sys.stderr)
             sys.exit(1)
 
     original_requirements = options.requirements
 
     # Alias functions to run pip locally or on the remote host
     if options.remote_pip:
-        run_pip = run
+        run_pip = functools.partial(run, stdout=sys.stderr)
         mkdtemp = remote_mkdtemp
         listdir = remote_listdir
         rmtree = remote_rmtree
         put_package = remote_move
         move = remote_move
         # Upload requirements files to a temp directory
-        print '-' * 78
-        print 'Uploading requirements...'
+        print(SEPARATOR, file=sys.stderr)
+        print('Uploading requirements...', file=sys.stderr)
         temp_dir = remote_mkdtemp(prefix=TEMPFILES_PREFIX)
-        atexit.register(run, 'rm -rf %s' % temp_dir)
+        atexit.register(run, 'rm -rf %s' % temp_dir, stdout=sys.stderr)
         remote_requirements = []
         for i, requirement in enumerate(options.requirements):
             req_dir = op.join(temp_dir, str(i))
-            run('mkdir %s' % req_dir)
+            run('mkdir %s' % req_dir, stdout=sys.stderr)
             remote_requirements.extend(put(requirement, req_dir))
         options.requirements = remote_requirements
         output_dir = op.join(temp_dir, 'packages')
-        run('mkdir %s' % output_dir)
-        print
+        run('mkdir %s' % output_dir, stdout=sys.stderr)
+        print(file=sys.stderr)
     else:
-        run_pip = functools.partial(subprocess.check_call, shell=True)
+        run_pip = functools.partial(subprocess.check_call, shell=True,
+                stdout=sys.stderr)
         mkdtemp = tempfile.mkdtemp
         listdir = os.listdir
         rmtree = shutil.rmtree
@@ -95,8 +102,8 @@ def main():
         move = local_move
 
     # Download packages
-    print '-' * 78
-    print 'Downloading packages...'
+    print(SEPARATOR, file=sys.stderr)
+    print('Downloading packages...', file=sys.stderr)
     requirements_packages = []
     for original_requirement, requirement in zip(
             original_requirements, options.requirements):
@@ -105,49 +112,51 @@ def main():
         run_pip('pip install -r %s --download %s' % (requirement, temp_dir))
         requirements_packages.append((original_requirement, listdir(temp_dir)))
         move(op.join(temp_dir, '*'), output_dir)
-    print
+    print(file=sys.stderr)
 
     # Upload or move packages to their final destination
     if options.upload:
-        print '-' * 78
+        print(SEPARATOR, file=sys.stderr)
         if options.remote_pip:
-            print 'Moving packages to their final destination...'
+            print('Moving packages to their final destination...', 
+                    file=sys.stderr)
         else:
-            print 'Uploading packages...'
+            print('Uploading packages...', file=sys.stderr)
         packages = [op.join(output_dir, p) for p in listdir(output_dir)]
         created_dirs = set()
         for package in packages:
             distro = list(distros_for_filename(package))[0]
             dst_dir = op.join(remote_dir, distro.key)
             if dst_dir not in created_dirs:
-                run('mkdir -p %s' % dst_dir)
+                run('mkdir -p %s' % dst_dir, stdout=sys.stderr)
                 created_dirs.add(dst_dir)
             put_package(package, dst_dir)
-    print
+    print(file=sys.stderr)
 
     # Print frozen requirements for each input requirements file
-    print '-' * 78
+    print(SEPARATOR, file=sys.stderr)
     for requirements_file, packages in requirements_packages:
-        print 'Frozen requirements for "%s":' % requirements_file
-        print 
+        print('# Frozen requirements for "%s":' % requirements_file)
+        print()
         for package in packages:
             distro = list(distros_for_filename(package))[0]
-            print '%s==%s' % (distro.key, distro.version)
-        print
+            print('%s==%s' % (distro.key, distro.version))
+        print()
 
 
 def remote_move(src, dst):
     """
     Move a file on a remote host.
     """
-    run('mv -fv %s %s' % (src, dst))
+    run('mv -fv %s %s' % (src, dst), stdout=sys.stderr)
 
 
 def local_move(src, dst):
     """
     Move a file on local host.
     """
-    subprocess.call('mv -fv %s %s' % (src, dst), shell=True)
+    subprocess.check_call('mv -fv %s %s' % (src, dst), shell=True,
+            stdout=sys.stderr)
 
 
 def remote_mkdtemp(prefix='', dir='/tmp'):
@@ -157,14 +166,14 @@ def remote_mkdtemp(prefix='', dir='/tmp'):
     while True:
         temp_dir = op.join(dir, '%s%s' % (prefix, uuid.uuid4().hex))
         if not exists(temp_dir):
-            run('mkdir %s' % temp_dir)
+            run('mkdir %s' % temp_dir, stdout=sys.stderr)
             break
     return temp_dir
 
 
 def remote_listdir(location):
-    return run('ls %s' % location).split()
+    return run('ls %s' % location, stdout=sys.stderr).split()
 
 
 def remote_rmtree(location):
-    run('rm -rf %s' % location)
+    run('rm -rf %s' % location, stdout=sys.stderr)
