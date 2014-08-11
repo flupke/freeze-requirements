4a8fc869be5529785c5ca612f8f8c09cf89840cc
Luper Rouch <luper.rouch@gmail.com>
2014-08-11 15:41:41 +0200

    cache filenames no longer include the path of the source file, just the hash the hash of its contents

--- freezerequirements/utils.py
+++ freezerequirements/utils.py
@@ -1,5 +1,4 @@
 import os.path as op
-import urllib
 import string
 import hashlib
 import os
@@ -10,12 +9,12 @@ from setuptools.package_index import distros_for_filename
 
 
 def likely_distro(filename):
-    """
+    '''
     Get the first distro as returned by :func:`distros_for_filename` that has a
     version that starts with a number.
-    """
-    distros = [d for d in distros_for_filename(filename) 
-            if d.version 
+    '''
+    distros = [d for d in distros_for_filename(filename)
+            if d.version
             and d.version[0] in string.digits]
     if len(distros) < 1:
         raise ValueError("can't find distro for %s" % filename)
@@ -23,38 +22,36 @@ def likely_distro(filename):
 
 
 def file_hash(filename):
-    """
+    '''
     Return the hash of *filename* contents.
-    """
+    '''
     with open(filename) as fp:
         return hashlib.sha1(fp.read()).hexdigest()
 
 
 def cache_dir():
-    """
+    '''
     Return the application's cache directory.
-    """
+    '''
     cache_dir = os.environ.get('XDG_CACHE_HOME', op.expanduser('~/.cache'))
     return op.join(cache_dir, 'freeze-requirements')
 
 
 def cache_path(filename):
-    """
+    '''
     Get cache path for *filename*.
-    """
-    filename = op.abspath(filename)
-    key = "%s-%s" % (urllib.quote_plus(filename), file_hash(filename))
-    return op.join(cache_dir(), key)
+    '''
+    return op.join(cache_dir(), file_hash(filename))
 
 
 def group_and_select_packages(packages_groups):
-    """
+    '''
     Group the list of packages filenames lists *packages_groups* by
     distribution key, and sort them by version number, highest first.
 
     Returns a ``{distro_key: [distro_version1, distro_version2, ...],
     distro_key2: ...}`` mapping.
-    """
+    '''
     grouped_packages = defaultdict(list)
     for packages in packages_groups:
         for package in packages:
@@ -70,7 +67,7 @@ def group_and_select_packages(packages_groups):
 
 class StringWithAttrs(unicode):
     '''
-    A mutable unicode subclass.
+    An unicode subclass, to be able to add attributes.
     '''
 
     pass
